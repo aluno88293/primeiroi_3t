02c698189335c1919555462ac0872535e550413f
beatriz <110063130+aluno88293@users.noreply.github.com>
2022-10-18 17:43:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,64 +1,75 @@
-// alert("Oi! Eu sou o Alerta");
+// alert("Oi! Eu sou o Alert");
 // document.write("Estou na página.");
-// console.log("Eu não estou no console.");
-deixe  tabuada  =  5 ;
-função  alo ( ) {
+// console.log("Eu estou no console.");
+let tabuada = 5;
+function alo(){
 
-    deixe  tabuada  =  documento . getElementById ( "entrada" ) . valor ;
+    let tabuada = document.getElementById("entrada").value;
 
-    documento . escreva ( "<h1>Tabuada do "  +  tabuada  +  "</h1>" ) ;
-    documento . escreva ( tabuada  +  " x 1 = "  +  ( tabuada * 1 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 2 = "  +  ( tabuada * 2 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 3 = "  +  ( tabuada * 3 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 4 = "  +  ( tabuada * 4 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 5 = "  +  ( tabuada * 5 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 6 = "  +  ( tabuada * 6 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 7 = "  +  ( tabuada * 7 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 8 = "  +  ( tabuada * 8 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 9 = "  +  ( tabuada * 9 ) + "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 10 = "  +  ( tabuada * 10 ) ) ;
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10));
 }
 // for(inicio, validaçao, manutençao)
-função  escreva ( ) {
-    for ( var  i = 1 ;  i  <=  10 ;  i ++ ) {
-        for ( var  j  =  1 ;  j  <= 10 ;  j ++ ) {
-            documento . escreva ( i  +  "x"  +  j  +  " = "  +  ( j * i ) + "<br>" ) ;
+function escreva(){
+    for(var i=1; i <= 10; i++){
+        for(var j = 1; j <=10; j++){
+            document.write(i + " x " + j + " = " + (j*i)+"<br>");
         }
-        documento . escreva ( "<br>" ) ;
+        document.write("<br>");
     }
 }
 
-função  quadrado ( ) {
-    for ( var  i  =  2 ;  i  <  11 ;  i ++ ) {
-        documento . write ( "O quadrado de "  +  i  +  " é "  +  ( i * i ) + "<br>" ) ;
+function quadrado(){
+    for(var i = 2; i < 11; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
     }
 }
 
-função  total ( ) {
-    deixe  val  =  documento . getElementById ( "valor" ) . valor ;
-    deixe  ju  =  documento . getElementById ( "juros" ) . valor ;
-    let  resultado  =  ( val  *  ( ( ju / 100 ) + 1 ) ) ;
-    documento . escreva ( "O total é de:"  +  resultado ) ;
-}
-// Para dividir usa a/
-// Calcula a média das 4 notas. Somas as 4 notas e dividir por 4.
-// Quanto falta para passar. pega 240 e subtrair como notas.
-função  soma ( ) {
-    seja  n1  =  documento . getElementById ( "b1" ) .  valor ;
-    seja  n2  =  documento . getElementById ( "b2" ) .  valor ;
-    seja  n3  =  documento . getElementById ( "b3" ) .  valor ;
-    deixe  n4  =  documento . getElementById ( "b4" ) .  valor ;
-    let  r  =  Número ( n1 )  +  Número ( n2 )  +  Número ( n3 )  +  Número ( n4 ) ;
-    documento . getElementById ( "resultado" ) . innerHTML  =  r ;
+function total(){
+    let val = document.getElementById("valor").value;
+    let ju = document.getElementById("juros").value;
+
+    if(!Number(val)){
+        alert("O valor deve ser um número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor"). focus();
+        return
+    }
+
+    if(!Number(ju)){
+        alert("O valor deve ser um número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor"). focus();
+        return
+    let resultado = (val * ((ju/100)+1));
+    document.write("O total é de: " + resultado);
+    }
+
+function soma(){
+    let n1 = document.getElementById("b1"). value;
+    let n2 = document.getElementById("b2"). value;
+    let n3 = document.getElementById("b3"). value;
+    let n4 = document.getElementById("b4"). value;
+    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4);
+    document.getElementById("resultado").innerHTML = r;
 
 }
-função  média ( ) {
-    seja  n1  =  documento . getElementById ( "b1" ) .  valor ;
-    seja  n2  =  documento . getElementById ( "b2" ) .  valor ;
-    seja  n3  =  documento . getElementById ( "b3" ) .  valor ;
-    deixe  n4  =  documento . getElementById ( "b4" ) .  valor ;
-    let  r  =  240  -  Número ( n1 )  +  Número ( n2 )  +  Número ( n3 )  +  Número ( n4 ) ;
-    documento . getElementById ( "resultado" ) . innerHTML  =  r ;
+function média(){
+    let n1 = document.getElementById("b1"). value;
+    let n2 = document.getElementById("b2"). value;
+    let n3 = document.getElementById("b3"). value;
+    let n4 = document.getElementById("b4"). value;
+    let r = 240 - Number(n1) + Number(n2) + Number(n3) + Number(n4);
+    document.getElementById("resultado").innerHTML = r;
 
     
 }
